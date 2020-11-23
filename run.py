c87d3dda315a48a11e7b5c1092724f70e2747f14
hikeolin <hikenike6@gmail.com>
2020-11-23 10:43:29 +0100

    first test run of bot weenec

--- run.py
+++ run.py
@@ -1,37 +1,46 @@
 import pika, os, time, logging 
+import json
 from chatterbot import ChatBot
 from bot import TwitterBot, Platform
 from config import create_api, create_queue
+from utils import train_bot, create_or_restore_bot_instance
 
 logger = logging.getLogger()
 
 def recieve_tweets(platform: Platform):
     producer_channel = create_queue('twitter_mentions')
     consumer_channel = create_queue('twitter_mentions')
+    
     def callback(ch, method, properties, body):
-        platform.perform_action(msg)
+        platform.perform_action(json.loads(body))
 
     # set up subscription on the queue
     consumer_channel.basic_consume('twitter_mentions', callback,
     auto_ack=True)
+    
     while True:
         try:
             messages = platform.get_intent()
+            print("messages - ", messages)
             for msg in messages:
-                logger.info(msg, type(msg))
-                producer_channel.basic_publish(exchange='', routing_key='twitter_mentions', body=msg)
+                logger.debug(msg, type(msg))
+                # print(msg, type(msg))
+                producer_channel.basic_publish(exchange='', routing_key='twitter_mentions', 
+                    body=json.dumps(msg))
             # time.sleep(7200)
-            time.sleep(300)
+            time.sleep(500)
             consumer_channel.start_consuming()
         except Exception as e:
             logger.debug(e)
-        finally:
-            producer_channel.close()
-            consumer_channel.close()
+            print(e)        
+            # producer_channel.close()
+            # consumer_channel.close()
+
 
 if __name__ == '__main__':
-    bot = ChatBot('meenec')
-    platform = TwitterBot(bot=bot)
+    bot = create_or_restore_bot_instance()  
+    train_bot(bot, 'data/faq_corpus/')
+    platform = TwitterBot(bot=bot, api=create_api())
     recieve_tweets(platform)
    
 
--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 """
-    train.py trains bot conservations using  as  INEC FAQ data source
+    train.py trains bot conservations using  an INEC FAQ data source
     :url  https://www.inecnigeria.org/voter-education/faqs/
 """
 
-
-if __name__ == '__main__':
-    from chatterbot import ChatBot
-    from utils import train_bot
+import sys
+from chatterbot import ChatBot
+from utils import train_bot
     
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise Exception("corpus path was not specified")
     bot = ChatBot('weenec')
-    train_bot(bot=bot)
+    train_bot(bot,sys.argv[1])
